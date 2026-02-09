dca762479c8419c1ff93de2718395e072f89ff24
Khadaffe Abubakar Sulaiman <95871722+KCprsnlcc@users.noreply.github.com>
2026-02-10 04:27:39 +0800

    fix: remove unsupported smoothTouch property from Lenis config

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -24,7 +24,6 @@ export default function LenisProvider({ children }: LenisProviderProps) {
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
       wheelMultiplier: 1,
       touchMultiplier: 2,
       infinite: false,
